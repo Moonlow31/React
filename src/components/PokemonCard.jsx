839e6f4ea2b6aafc11418ce6065763431d4bcaea
Franck HERPOUX <franckherpoux@hotmail.fr>
2024-03-27 17:01:07 +0100

    Création d'un props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,8 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0]
+function PokemonCard({pokemon}) {
     return <figure>
-        <div>{pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>"???"</p>}</div>
+        <div>{pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>???</p>}</div>
         <figcaption>{pokemon.name}</figcaption>
     </figure>;
-
   }
-  //<img src={pokemon.imgSrc}></img>
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
   export default PokemonCard;
